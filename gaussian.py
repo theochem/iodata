b86fafa776ef321e1fd3df3d38e118acbd3158cc
Steven Vandenbrande <Steven.Vandenbrande@UGent.be>
2013-06-03 17:10:35 +0200

    Fix typo in load_fchk

--- gaussian.py
+++ gaussian.py
@@ -270,7 +270,7 @@ def load_fchk(filename, lf):
     from horton.gbasis import GOBasis
 
     fchk = FCHKFile(filename, [
-        "Number of electrons", "Number of independant functions",
+        "Number of electrons", "Number of independent functions",
         "Number of alpha electrons", "Number of beta electrons",
         "Atomic numbers", "Current cartesian coordinates",
         "Shell types", "Shell to atom map", "Shell to atom map",
@@ -363,7 +363,7 @@ def load_fchk(filename, lf):
     permutation = np.array(permutation, dtype=int)
 
     # C) Load the wavefunction
-    nbasis_indep = fchk.fields.get("Number of independant functions")
+    nbasis_indep = fchk.fields.get("Number of independent functions")
     if nbasis_indep is None:
         nbasis_indep = obasis.nbasis
     if 'Beta Orbital Energies' in fchk.fields:
