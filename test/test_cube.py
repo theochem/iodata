4a30b39ece8fe2849ab985c590ce2b4e4c8c18c5
Matthew Chan <chanmhy@mcmaster.ca>
2013-11-17 22:54:34 -0500

    added pylint ignore to test files

--- test/test_cube.py
+++ test/test_cube.py
@@ -18,6 +18,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 #--
+#pylint: skip-file
 
 
 import numpy as np
--- test/test_gaussian.py
+++ test/test_gaussian.py
@@ -18,6 +18,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 #--
+#pylint: skip-file
 
 
 from nose.tools import assert_raises
