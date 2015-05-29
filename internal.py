477778cec7c959509db46bf4b4214e9adae52d7f
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2015-05-29 07:06:20 -0700

    Updated headers

--- internal.py
+++ internal.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Horton is a development platform for electronic structure methods.
-# Copyright (C) 2011-2013 Toon Verstraelen <Toon.Verstraelen@UGent.be>
+# Copyright (C) 2011-2015 The Horton Development Team
 #
 # This file is part of Horton.
 #
