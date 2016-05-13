70f9071f4b631d624216ea3ce3928a6a76f36a4b
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-05-13 11:49:48 +0200

    Read all-electron WFN and uncontracted basis from CP2K

--- cp2k.py
+++ cp2k.py
@@ -18,7 +18,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
-'''CP2K atomic wavefunctions'''
+"""CP2K atomic wavefunctions"""
 
 import numpy as np
 
@@ -29,7 +29,214 @@ from horton.gbasis.cext import GOBasis, fac2
 __all__ = ['load_atom_cp2k']
 
 
-def _read_coeffs_helper(f, oe):
+def _get_cp2k_norm_corrections(l, alphas):
+    """Compute the corrections for the normalization of the basis functions.
+
+    This correction is needed because the CP2K atom code works with non-normalized basis
+    functions. HORTON assumes Gaussian primitives are always normalized.
+
+    Parameters
+    ----------
+    l : int
+        The angular momentum of the (pure) basis function. (s=0, p=1, ...)
+    alphas : float or np.ndarray
+             The exponent or exponents of the Gaussian primitives for which the correction
+             is to be computed.
+
+    Returns
+    -------
+    corrections : float or np.ndarray
+                  The scale factor for the expansion coefficients of the wavefunction in
+                  terms of primitive Gaussians. The inverse of this correction can be
+                  applied to the contraction coefficients.
+    """
+    expzet = 0.25*(2*l + 3)
+    prefac = np.sqrt(np.sqrt(np.pi)/2.0**(l + 2)*fac2(2*l + 1))
+    zeta = 2.0*alphas
+    return zeta**expzet/prefac
+
+
+def _read_cp2k_contracted_obasis(f):
+    """Read a contracted basis set from an open CP2K ATOM output file.
+
+    Parameters
+    ----------
+    f : file
+        An open readable file object.
+
+    Returns
+    -------
+    obasis : GOBasis
+             The orbital basis read from the file.
+    """
+    # Load the relevant data from the file
+    basis_desc = []
+    for line in f:
+        if line.startswith(' *******************'):
+            break
+        elif line[3:12] == 'Functions':
+            shell_type = str_to_shell_types(line[1:2], pure=True)[0]
+            a = []  # exponents (alpha)
+            c = []  # contraction coefficients
+            basis_desc.append((shell_type, a, c))
+        else:
+            values = [float(w) for w in line.split()]
+            a.append(values[0])   # one exponent per line
+            c.append(values[1:])  # many contraction coefficients per line
+
+    # Convert the basis into HORTON format
+    shell_map = []
+    shell_types = []
+    nprims = []
+    alphas = []
+    con_coeffs = []
+
+    for shell_type, a, c in basis_desc:
+        # get correction to contraction coefficients. CP2K uses different normalization
+        # conventions.
+        corrections = _get_cp2k_norm_corrections(abs(shell_type), np.array(a))
+        c = np.array(c)/corrections.reshape(-1, 1)
+        # fill in arrays
+        for col in c.T:
+            shell_map.append(0)
+            shell_types.append(shell_type)
+            nprims.append(len(col))
+            alphas.extend(a)
+            con_coeffs.extend(col)
+
+    # Create the basis object
+    coordinates = np.zeros((1, 3))
+    shell_map = np.array(shell_map)
+    nprims = np.array(nprims)
+    shell_types = np.array(shell_types)
+    alphas = np.array(alphas)
+    con_coeffs = np.array(con_coeffs)
+    obasis = GOBasis(coordinates, shell_map, nprims, shell_types, alphas, con_coeffs)
+    return obasis
+
+
+def _read_cp2k_uncontracted_obasis(f):
+    """Read an uncontracted basis set from an open CP2K ATOM output file.
+
+    Parameters
+    ----------
+    f : file
+        An open readable file object.
+
+    Returns
+    -------
+    obasis : GOBasis
+             The orbital basis read from the file.
+    """
+    # Load the relevant data from the file
+    basis_desc = []
+    shell_type = None
+    for line in f:
+        if line.startswith(' *******************'):
+            break
+        elif line[3:13] == 'Exponents:':
+            shell_type = str_to_shell_types(line[1:2], pure=True)[0]
+        words = line.split()
+        if len(words) >= 2:
+            # read the exponent
+            alpha = float(words[-1])
+            basis_desc.append((shell_type, alpha))
+
+    # Convert the basis into HORTON format
+    shell_map = []
+    shell_types = []
+    nprims = []
+    alphas = []
+    con_coeffs = []
+
+    # fill in arrays
+    for shell_type, alpha in basis_desc:
+        correction = _get_cp2k_norm_corrections(abs(shell_type), alpha)
+        shell_map.append(0)
+        shell_types.append(shell_type)
+        nprims.append(1)
+        alphas.append(alpha)
+        con_coeffs.append(1.0 / correction)
+
+    # Create the basis object
+    centers = np.zeros((1, 3))
+    shell_map = np.array(shell_map)
+    nprims = np.array(nprims)
+    shell_types = np.array(shell_types)
+    alphas = np.array(alphas)
+    con_coeffs = np.array(con_coeffs)
+    obasis = GOBasis(centers, shell_map, nprims, shell_types, alphas, con_coeffs)
+    return obasis
+
+
+def _read_cp2k_obasis(f):
+    """Read a basis set from an open CP2K ATOM output file."""
+    f.next()         # Skip empty line
+    line = f.next()  # Check for contracted versus uncontracted
+    if line == ' ********************** Contracted Gaussian Type Orbitals '\
+               '**********************\n':
+        return _read_cp2k_contracted_obasis(f)
+    elif line == ' ********************* Uncontracted Gaussian Type Orbitals '\
+                 '*********************\n':
+        return _read_cp2k_uncontracted_obasis(f)
+    else:
+        raise IOError('Could not find basis set in CP2K ATOM output.')
+
+
+def _read_cp2k_occupations_energies(f, restricted):
+    """Read orbital occupation numbers and energies from an open CP2K ATOM output file.
+
+    Parameters
+    ----------
+    f : file
+        An open readable file object.
+    restricted : bool
+                 Is wavefunction restricted or unrestricted?
+
+    Returns
+    -------
+    oe_alpha, oe_beta : list
+                        A list with orbital properties. Each element is a tuple with the
+                        following info: (angular_momentum l, spin component: 'alpha' or
+                        'beta', occupation number, orbital energy).
+    """
+    oe_alpha = []
+    oe_beta = []
+    empty = 0
+    while empty < 2:
+        line = f.next()
+        words = line.split()
+        if len(words) == 0:
+            empty += 1
+            continue
+        empty = 0
+        s = int(words[0])
+        l = int(words[2 - restricted])
+        occ = float(words[3 - restricted])
+        ener = float(words[4 - restricted])
+        if restricted or words[1] == 'alpha':
+            oe_alpha.append((l, s, occ, ener))
+        else:
+            oe_beta.append((l, s, occ, ener))
+    return oe_alpha, oe_beta
+
+
+def _read_cp2k_orbital_coeffs(f, oe):
+    """Read the expansion coefficients of the orbital from an open CP2K ATOM output.
+
+    Parameters
+    ----------
+    f : file
+        An open readable file object.
+    oe : list
+         The orbital occupation numbers and energies read with
+         ``_read_cp2k_occupations_energies``.
+
+    Returns
+    -------
+    result : dict
+             Key is an (l, s) pair and value is an array with orbital coefficients.
+    """
     coeffs = {}
     f.next()
     while len(coeffs) < len(oe):
@@ -48,122 +255,125 @@ def _read_coeffs_helper(f, oe):
     return coeffs
 
 
-def _helper_norb(oe):
+def _get_norb_nel(oe):
+    """Return number of orbitals and electrons.
+
+    Parameters
+    ----------
+    oe : list
+         The orbital occupation numbers and energies read with
+         ``_read_cp2k_occupations_energies``.
+    """
     norb = 0
     nel = 0
-    for l, s, occ, ener in oe:
-        norb += 2*l+1
-        nel += occ
+    for row in oe:
+        norb += 2*row[0] + 1
+        nel += row[2]
     return norb, nel
 
 
-def _helper_exp(exp, oe, coeffs, shell_types, restricted):
+def _fill_exp(exp, oe, coeffs, shell_types, restricted):
+    """Fill in orbital coefficients, energies and occupation numbers in ``exp``.
+
+    Parameters
+    ----------
+    exp : DenseExpansion
+          An object to represent the orbitals
+    oe : list
+         The orbital occupation numbers and energies read with
+         ``_read_cp2k_occupations_energies``.
+    coeffs : dict
+             The orbital coefficients read with ``_read_cp2k_orbital_coeffs``.
+    shell_types : np.ndarray
+                  The array with shell types of the GOBasis instance.
+    restricted : bool
+                 Is wavefunction restricted or unrestricted?
+    """
     # Find the offsets for each angular momentum
     offset = 0
     offsets = []
     ls = abs(shell_types)
     for l in sorted(set(ls)):
         offsets.append(offset)
-        offset += (2*l+1)*(l == ls).sum()
+        offset += (2*l + 1)*(l == ls).sum()
     del offset
 
     # Fill in the coefficients
     iorb = 0
     for l, s, occ, ener in oe:
         cs = coeffs.get((l, s))
-        if cs is None:
-            assert occ == 0
-            continue
-        stride = 2*l+1
+        # The following no longer seems relevant?
+        # if cs is None:
+        #     assert occ == 0
+        #     continue
+        stride = 2*l + 1
         for m in xrange(-l, l+1):
             im = m + l
             exp.energies[iorb] = ener
-            exp.occupations[iorb] = im < occ/(restricted+1)
+            exp.occupations[iorb] = occ/float((restricted + 1)*(2*l + 1))
             for ic in xrange(len(cs)):
-                exp.coeffs[offsets[l] + stride*ic + im,iorb] = cs[ic]
+                exp.coeffs[offsets[l] + stride*ic + im, iorb] = cs[ic]
             iorb += 1
 
 
-def _get_cp2k_norm_corrections(l, alphas):
-    expzet = 0.25*(2*l + 3)
-    prefac = np.sqrt(np.sqrt(np.pi)/2.0**(l+2)*fac2(2*l+1))
-    zeta = 2*np.array(alphas)
-    return zeta**expzet/prefac
-
-
 def load_atom_cp2k(filename, lf):
-    '''Load data from a CP2K ATOM computation
+    """Load data from a CP2K ATOM computation.
 
-       **Arguments:**
+    Parameters
+    ---------
 
-       filename
-            The name of the cp2k out file
+    filename : str
+               The name of the cp2k out file
+    lf : LinalgFactory
+         A linear-algebra factory.
 
-       **Returns** a dictionary with ``obasis``, ``exp_alpha``, ``coordinates``,
-       ``numbers``, ``energy``, ``pseudo_numbers``. The dictionary may also
-       contain: ``exp_beta``.
-    '''
+    Returns
+    -------
+    results : dict
+              Contains: ``obasis``, ``exp_alpha``, ``coordinates``, ``numbers``,
+              ``energy``, ``pseudo_numbers``. May contain: ``exp_beta``.
+
+
+    Notes
+    -----
+
+    This function assumes that the following subsections are present in the CP2K
+    ATOM input file, in the section ``ATOM%PRINT``:
+
+    .. code-block:: text
+
+      &PRINT
+        &POTENTIAL
+        &END POTENTIAL
+        &BASIS_SET
+        &END BASIS_SET
+        &ORBITALS
+        &END ORBITALS
+      &END PRINT
+    """
     with open(filename) as f:
         # Find the element number
+        number = None
         for line in f:
             if line.startswith(' Atomic Energy Calculation'):
                 number = int(line[-5:-1])
                 break
+        if number is None:
+            raise IOError('Could not find atomic number in CP2K ATOM output: %s.' % filename)
+
+        # Go to the pseudo basis set
+        for line in f:
+            if line.startswith(' All Electron Basis'):
+                break
+
+        ae_obasis = _read_cp2k_obasis(f)
 
         # Go to the pseudo basis set
         for line in f:
             if line.startswith(' Pseudopotential Basis'):
                 break
 
-        f.next() # empty line
-        line = f.next() # Check for GTO
-        assert line == ' ********************** Contracted Gaussian Type Orbitals **********************\n'
-
-        # Load the basis used for the PP wavefn
-        basis_desc = []
-        for line in f:
-            if line.startswith(' *******************'):
-                break
-            elif line[3:12] == 'Functions':
-                shell_type = str_to_shell_types(line[1:2], pure=True)[0]
-                a = []
-                c = []
-                basis_desc.append((shell_type, a, c))
-            else:
-                values = [float(w) for w in line.split()]
-                a.append(values[0])
-                c.append(values[1:])
-
-        # Convert the basis into HORTON format
-        shell_map = []
-        shell_types = []
-        nprims = []
-        alphas = []
-        con_coeffs = []
-
-        for shell_type, a, c in basis_desc:
-            # get correction to contraction coefficients.
-            corrections = _get_cp2k_norm_corrections(abs(shell_type), a)
-            c = np.array(c)/corrections.reshape(-1,1)
-            # fill in arrays
-            for col in c.T:
-                shell_map.append(0)
-                shell_types.append(shell_type)
-                nprims.append(len(col))
-                alphas.extend(a)
-                con_coeffs.extend(col)
-
-        # Create the basis object
-        coordinates = np.zeros((1, 3))
-        shell_map = np.array(shell_map)
-        nprims = np.array(nprims)
-        shell_types = np.array(shell_types)
-        alphas = np.array(alphas)
-        con_coeffs = np.array(con_coeffs)
-        obasis = GOBasis(coordinates, shell_map, nprims, shell_types, alphas, con_coeffs)
-        if lf.default_nbasis is not None and lf.default_nbasis != obasis.nbasis:
-            raise TypeError('The value of lf.default_nbasis does not match nbasis reported in the cp2k.out file.')
-        lf.default_nbasis = obasis.nbasis
+        pp_obasis = _read_cp2k_obasis(f)
 
         # Search for (un)restricted
         restricted = None
@@ -177,12 +387,28 @@ def load_atom_cp2k(filename, lf):
                     break
 
         # Search for the core charge (pseudo number)
+        pseudo_number = None
         for line in f:
             if line.startswith('          Core Charge'):
                 pseudo_number = float(line[70:])
                 assert pseudo_number == int(pseudo_number)
-                pseudo_number = int(pseudo_number)
                 break
+            elif line.startswith(' Electronic structure'):
+                pseudo_number = float(number)
+                break
+        if pseudo_number is None:
+            raise IOError('Could not find effective core charge in CP2K ATOM output:'
+                          ' %s' % filename)
+
+        # Select the correct basis
+        if pseudo_number == number:
+            obasis = ae_obasis
+        else:
+            obasis = pp_obasis
+        if lf.default_nbasis is not None and lf.default_nbasis != obasis.nbasis:
+            raise IOError('The value of lf.default_nbasis does not match nbasis '
+                          'reported in CP2K ATOM output: %s' % filename)
+        lf.default_nbasis = obasis.nbasis
 
         # Search for energy
         for line in f:
@@ -195,61 +421,44 @@ def load_atom_cp2k(filename, lf):
             if line.startswith(' Orbital energies'):
                 break
         f.next()
-
-        oe_alpha = []
-        oe_beta = []
-        empty = 0
-        while empty < 2:
-            line = f.next()
-            words = line.split()
-            if len(words) == 0:
-                empty += 1
-                continue
-            empty = 0
-            s = int(words[0])
-            l = int(words[2-restricted])
-            occ = float(words[3-restricted])
-            ener = float(words[4-restricted])
-            if restricted or words[1] == 'alpha':
-                oe_alpha.append((l, s, occ, ener))
-            else:
-                oe_beta.append((l, s, occ, ener))
+        oe_alpha, oe_beta = _read_cp2k_occupations_energies(f, restricted)
 
         # Read orbital expansion coefficients
         line = f.next()
-        assert (line == " Atomic orbital expansion coefficients [Alpha]\n") or \
-               (line == " Atomic orbital expansion coefficients []\n")
-
-        coeffs_alpha = _read_coeffs_helper(f, oe_alpha)
+        if (line != " Atomic orbital expansion coefficients [Alpha]\n") and \
+           (line != " Atomic orbital expansion coefficients []\n"):
+            raise IOError('Could not find orbital coefficients in CP2K ATOM output: '
+                          '%s' % filename)
+        coeffs_alpha = _read_cp2k_orbital_coeffs(f, oe_alpha)
 
         if not restricted:
             line = f.next()
-            assert (line == " Atomic orbital expansion coefficients [Beta]\n")
-
-            coeffs_beta = _read_coeffs_helper(f, oe_beta)
-
+            if line != " Atomic orbital expansion coefficients [Beta]\n":
+                raise IOError('Could not find beta orbital coefficient in CP2K ATOM '
+                              'output: %s' % filename)
+            coeffs_beta = _read_cp2k_orbital_coeffs(f, oe_beta)
 
         # Turn orbital data into a HORTON orbital expansions
         if restricted:
-            norb, nel = _helper_norb(oe_alpha)
-            assert nel%2 == 0
+            norb, nel = _get_norb_nel(oe_alpha)
+            assert nel % 2 == 0
             exp_alpha = lf.create_expansion(obasis.nbasis, norb)
             exp_beta = None
-            _helper_exp(exp_alpha, oe_alpha, coeffs_alpha, shell_types, restricted)
+            _fill_exp(exp_alpha, oe_alpha, coeffs_alpha, obasis.shell_types, restricted)
         else:
-            norb_alpha, nalpha = _helper_norb(oe_alpha)
-            norb_beta, nbeta = _helper_norb(oe_beta)
+            norb_alpha = _get_norb_nel(oe_alpha)[0]
+            norb_beta = _get_norb_nel(oe_beta)[0]
             assert norb_alpha == norb_beta
             exp_alpha = lf.create_expansion(obasis.nbasis, norb_alpha)
             exp_beta = lf.create_expansion(obasis.nbasis, norb_beta)
-            _helper_exp(exp_alpha, oe_alpha, coeffs_alpha, shell_types, restricted)
-            _helper_exp(exp_beta, oe_beta, coeffs_beta, shell_types, restricted)
+            _fill_exp(exp_alpha, oe_alpha, coeffs_alpha, obasis.shell_types, restricted)
+            _fill_exp(exp_beta, oe_beta, coeffs_beta, obasis.shell_types, restricted)
 
     result = {
         'obasis': obasis,
         'lf': lf,
         'exp_alpha': exp_alpha,
-        'coordinates': coordinates,
+        'coordinates': obasis.centers,
         'numbers': np.array([number]),
         'energy': energy,
         'pseudo_numbers': np.array([pseudo_number]),
--- test/test_cp2k.py
+++ test/test_cp2k.py
@@ -18,46 +18,184 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
+"""Tests for CP2K ATOM output reader"""
 
 
-import numpy as np
+from nose.tools import assert_raises
 
 from horton import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from horton.test.common import truncated_file
+
+
+def check_orthonormality(mol):
+    """Helper function to test if the orbitals are orthonormal"""
+    olp = mol.obasis.compute_overlap(mol.lf)
+    mol.exp_alpha.check_orthonormality(olp)
+    if hasattr(mol, 'exp_beta'):
+        mol.exp_beta.check_orthonormality(olp)
 
 
 def test_atom_si_uks():
     fn_out = context.get_fn('test/atom_si.cp2k.out')
     mol = IOData.from_file(fn_out)
-
+    assert mol.obasis.nbasis == 13
     assert (mol.numbers == [14]).all()
     assert (mol.pseudo_numbers == [4]).all()
-    assert (mol.exp_alpha.occupations == [1, 1, 1, 0]).all()
+    assert (mol.exp_alpha.occupations == [1, 2.0/3.0, 2.0/3.0, 2.0/3.0]).all()
     assert (mol.exp_beta.occupations == [1, 0, 0, 0]).all()
     assert abs(mol.exp_alpha.energies - [-0.398761, -0.154896, -0.154896, -0.154896]).max() < 1e-4
     assert abs(mol.exp_beta.energies - [-0.334567, -0.092237, -0.092237, -0.092237]).max() < 1e-4
     assert abs(mol.energy - -3.761587698067) < 1e-10
     assert (mol.obasis.shell_types == [0, 0, 1, 1, -2]).all()
-
-    olp = mol.obasis.compute_overlap(mol.lf)
-    ca = mol.exp_alpha.coeffs
-    cb = mol.exp_beta.coeffs
-    assert abs(np.diag(olp._array[:2,:2]) - np.array([0.42921199338707744, 0.32067871530183140])).max() < 1e-5
-    assert abs(np.dot(ca.T, np.dot(olp._array, ca)) - np.identity(4)).max() < 1e-5
-    assert abs(np.dot(cb.T, np.dot(olp._array, cb)) - np.identity(4)).max() < 1e-5
-
+    check_orthonormality(mol)
 
 
 def test_atom_o_rks():
     fn_out = context.get_fn('test/atom_om2.cp2k.out')
     mol = IOData.from_file(fn_out)
-
+    assert mol.obasis.nbasis == 13
     assert (mol.numbers == [8]).all()
     assert (mol.pseudo_numbers == [6]).all()
     assert (mol.exp_alpha.occupations == [1, 1, 1, 1]).all()
     assert abs(mol.exp_alpha.energies - [0.102709, 0.606458, 0.606458, 0.606458]).max() < 1e-4
+    assert not hasattr(mol, 'exp_beta')
     assert abs(mol.energy - -15.464982778766) < 1e-10
     assert (mol.obasis.shell_types == [0, 0, 1, 1, -2]).all()
+    check_orthonormality(mol)
 
-    olp = mol.obasis.compute_overlap(mol.lf)
-    ca = mol.exp_alpha.coeffs
-    assert abs(np.dot(ca.T, np.dot(olp._array, ca)) - np.identity(4)).max() < 1e-5
+
+def test_carbon_gs_ae_contracted():
+    fn_out = context.get_fn('test/carbon_gs_ae_contracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 18
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [6]).all()
+    assert (mol.exp_alpha.occupations == [1, 1, 2.0/3.0, 2.0/3.0, 2.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-10.058194, -0.526244, -0.214978,
+                                       -0.214978, -0.214978]).all()
+    assert (mol.exp_beta.occupations == [1, 1, 0, 0, 0]).all()
+    assert (mol.exp_beta.energies == [-10.029898, -0.434300, -0.133323,
+                                      -0.133323, -0.133323]).all()
+    assert mol.energy == -37.836423363057
+    check_orthonormality(mol)
+
+
+def test_carbon_gs_ae_uncontracted():
+    fn_out = context.get_fn('test/carbon_gs_ae_uncontracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 640
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [6]).all()
+    assert (mol.exp_alpha.occupations == [1, 1, 2.0/3.0, 2.0/3.0, 2.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-10.050076, -0.528162, -0.217626,
+                                       -0.217626, -0.217626]).all()
+    assert (mol.exp_beta.occupations == [1, 1, 0, 0, 0]).all()
+    assert (mol.exp_beta.energies == [-10.022715, -0.436340, -0.137135,
+                                      -0.137135, -0.137135]).all()
+    assert mol.energy == -37.842552743398
+    check_orthonormality(mol)
+
+
+def test_carbon_gs_pp_contracted():
+    fn_out = context.get_fn('test/carbon_gs_pp_contracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 17
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [4]).all()
+    assert (mol.exp_alpha.occupations == [1, 2.0/3.0, 2.0/3.0, 2.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-0.528007, -0.219974, -0.219974, -0.219974]).all()
+    assert (mol.exp_beta.occupations == [1, 0, 0, 0]).all()
+    assert (mol.exp_beta.energies == [-0.429657, -0.127060, -0.127060, -0.127060]).all()
+    assert mol.energy == -5.399938535844
+    check_orthonormality(mol)
+
+
+def test_carbon_gs_pp_uncontracted():
+    fn_out = context.get_fn('test/carbon_gs_pp_uncontracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 256
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [4]).all()
+    assert (mol.exp_alpha.occupations == [1, 2.0/3.0, 2.0/3.0, 2.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-0.528146, -0.219803, -0.219803, -0.219803]).all()
+    assert (mol.exp_beta.occupations == [1, 0, 0, 0]).all()
+    assert (mol.exp_beta.energies == [-0.429358, -0.126411, -0.126411, -0.126411]).all()
+    assert mol.energy == -5.402288849332
+    check_orthonormality(mol)
+
+
+def test_carbon_sc_ae_contracted():
+    fn_out = context.get_fn('test/carbon_sc_ae_contracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 18
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [6]).all()
+    assert (mol.exp_alpha.occupations == [1, 1, 1.0/3.0, 1.0/3.0, 1.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-10.067251, -0.495823, -0.187878,
+                                       -0.187878, -0.187878]).all()
+    assert not hasattr(mol, 'exp_beta')
+    assert mol.energy == -37.793939631890
+    check_orthonormality(mol)
+
+
+def test_carbon_sc_ae_uncontracted():
+    fn_out = context.get_fn('test/carbon_sc_ae_uncontracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 640
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [6]).all()
+    assert (mol.exp_alpha.occupations == [1, 1, 1.0/3.0, 1.0/3.0, 1.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-10.062206, -0.499716, -0.192580,
+                                       -0.192580, -0.192580]).all()
+    assert not hasattr(mol, 'exp_beta')
+    assert mol.energy == -37.800453482378
+    check_orthonormality(mol)
+
+
+def test_carbon_sc_pp_contracted():
+    fn_out = context.get_fn('test/carbon_sc_pp_contracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 17
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [4]).all()
+    assert (mol.exp_alpha.occupations == [1, 1.0/3.0, 1.0/3.0, 1.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-0.500732, -0.193138, -0.193138, -0.193138]).all()
+    assert not hasattr(mol, 'exp_beta')
+    assert mol.energy == -5.350765755382
+    check_orthonormality(mol)
+
+
+def test_carbon_sc_pp_uncontracted():
+    fn_out = context.get_fn('test/carbon_sc_pp_uncontracted.cp2k.out')
+    mol = IOData.from_file(fn_out)
+    assert mol.obasis.nbasis == 256
+    assert (mol.numbers == [6]).all()
+    assert (mol.pseudo_numbers == [4]).all()
+    assert (mol.exp_alpha.occupations == [1, 1.0/3.0, 1.0/3.0, 1.0/3.0]).all()
+    assert (mol.exp_alpha.energies == [-0.500238, -0.192365, -0.192365, -0.192365]).all()
+    assert not hasattr(mol, 'exp_beta')
+    assert mol.energy == -5.352864672201
+    check_orthonormality(mol)
+
+
+def test_errors():
+    fn_test = context.get_fn('test/carbon_sc_pp_uncontracted.cp2k.out')
+    with truncated_file('horton.io.test.test_cp2k.test_errors', fn_test, 0, 0) as fn:
+        with assert_raises(IOError):
+            IOData.from_file(fn)
+    with truncated_file('horton.io.test.test_cp2k.test_errors', fn_test, 107, 10) as fn:
+        with assert_raises(IOError):
+            IOData.from_file(fn)
+    with truncated_file('horton.io.test.test_cp2k.test_errors', fn_test, 357, 10) as fn:
+        with assert_raises(IOError):
+            IOData.from_file(fn)
+    with truncated_file('horton.io.test.test_cp2k.test_errors', fn_test, 405, 10) as fn:
+        with assert_raises(IOError):
+            IOData.from_file(fn)
+    lf = DenseLinalgFactory(1)
+    with assert_raises(IOError):
+        IOData.from_file(fn_test, lf=lf)
+    fn_test = context.get_fn('test/carbon_gs_pp_uncontracted.cp2k.out')
+    with truncated_file('horton.io.test.test_cp2k.test_errors', fn_test, 456, 10) as fn:
+        with assert_raises(IOError):
+            IOData.from_file(fn)
