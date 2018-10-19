d0bb8b0d1806e33b8102253bf11d5264c513fb2d
Matthew Chan <chanmhy@mcmaster.ca>
2018-10-18 23:03:35 -0400

    added check for RTD in gen_docs

--- doc/gen_docs.sh
+++ doc/gen_docs.sh
@@ -2,4 +2,6 @@
 
 SPHINX_APIDOC_OPTIONS=members,undoc-members,show-inheritance,inherited-members sphinx-apidoc -o pyapi/ ../iodata/ ../iodata/test/test_*.py ../iodata/test/cached --separate
 
-make html
+if ! [[ -z ${READTHEDOCS} ]]; then
+  make html
+fi
