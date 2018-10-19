7e4a8f19b397abf7bf75cd417328b791e5ffad38
Matthew Chan <chanmhy@mcmaster.ca>
2018-10-18 23:10:23 -0400

    removed make html from gen_docs

--- doc/gen_docs.sh
+++ doc/gen_docs.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 SPHINX_APIDOC_OPTIONS=members,undoc-members,show-inheritance,inherited-members sphinx-apidoc -o pyapi/ ../iodata/ ../iodata/test/test_*.py ../iodata/test/cached --separate
-
-if ! [[ -z ${READTHEDOCS} ]]; then
-  make html
-fi
