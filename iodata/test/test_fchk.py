c036315e6ee7e765b2785918cbe9f7dc3410ad2e
Fanwang Meng <fwmeng88@gmail.com>
2019-03-18 13:56:28 -0400

    Use np.testing for unit tests of fchk

--- iodata/test/test_fchk.py
+++ iodata/test/test_fchk.py
@@ -19,18 +19,19 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
-# pragma pylint: disable=invalid-name,fixme
+# pragma pylint: disable=invalid-name,no-member,fixme
 """Test iodata.fchk module."""
 
-
 import numpy as np
+from numpy.testing import assert_equal, assert_allclose
 
 import pytest
 
-from .. fchk import load
-from .. iodata import IOData
-from .. utils import shells_to_nbasis, check_dm
-from .. overlap import compute_overlap
+from ..fchk import load
+from ..iodata import IOData
+from ..utils import shells_to_nbasis, check_dm
+from ..overlap import compute_overlap
+
 try:
     from importlib_resources import path
 except ImportError:
@@ -54,16 +55,19 @@ def test_load_fchk_hf_sto3g_num():
     coordinates = fields['coordinates']
     numbers = fields['numbers']
     energy = fields['energy']
-    assert len(obasis["shell_types"]) == 4
-    assert shells_to_nbasis(obasis["shell_types"]) == 6
-    assert (obasis['nprims'] == 3).all()
-    assert len(coordinates) == len(numbers)
-    assert coordinates.shape[1] == 3
-    assert len(numbers) == 2
-    assert energy == -9.856961609951867E+01
-    assert (fields['mulliken_charges'] == [0.45000000E+00, 4.22300000E+00]).all()
-    assert (fields['npa_charges'] == [3.50000000E+00, 1.32000000E+00]).all()
-    assert (fields['esp_charges'] == [0.77700000E+00, 0.66600000E+00]).all()
+    assert_equal(len(obasis["shell_types"]), 4)
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 6)
+    assert_equal(obasis['nprims'], 3)
+    assert_equal(len(coordinates), len(numbers))
+    assert_equal(coordinates.shape[1], 3)
+    assert_equal(len(numbers), 2)
+    assert_allclose(energy, -9.856961609951867E+01)
+    assert_allclose(fields['mulliken_charges'],
+                    [0.45000000E+00, 4.22300000E+00])
+    assert_allclose(fields['npa_charges'],
+                    [3.50000000E+00, 1.32000000E+00])
+    assert_allclose(fields['esp_charges'],
+                    [0.77700000E+00, 0.66600000E+00])
 
 
 def test_load_fchk_h_sto3g_num():
@@ -74,13 +78,13 @@ def test_load_fchk_h_sto3g_num():
     coordinates = fields['coordinates']
     numbers = fields['numbers']
     energy = fields['energy']
-    assert len(obasis["shell_types"]) == 1
-    assert shells_to_nbasis(obasis["shell_types"]) == 1
-    assert (obasis['nprims'] == 3).all()
-    assert len(coordinates) == len(numbers)
-    assert coordinates.shape[1] == 3
-    assert len(numbers) == 1
-    assert energy == -4.665818503844346E-01
+    assert_equal(len(obasis["shell_types"]), 1)
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 1)
+    assert_equal(obasis['nprims'], 3)
+    assert_equal(len(coordinates), len(numbers))
+    assert_equal(coordinates.shape[1], 3)
+    assert_equal(len(numbers), 1)
+    assert_allclose(energy, -4.665818503844346E-01)
 
 
 def test_load_fchk_o2_cc_pvtz_pure_num():
@@ -90,12 +94,12 @@ def test_load_fchk_o2_cc_pvtz_pure_num():
     coordinates = fields['coordinates']
     numbers = fields['numbers']
     energy = fields['energy']
-    assert len(obasis["shell_types"]) == 20
-    assert shells_to_nbasis(obasis["shell_types"]) == 60
-    assert len(coordinates) == len(numbers)
-    assert coordinates.shape[1] == 3
-    assert len(numbers) == 2
-    assert energy == -1.495944878699246E+02
+    assert_equal(len(obasis["shell_types"]), 20)
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 60)
+    assert_equal(len(coordinates), len(numbers))
+    assert_equal(coordinates.shape[1], 3)
+    assert_equal(len(numbers), 2)
+    assert_allclose(energy, -1.495944878699246E+02)
 
 
 def test_load_fchk_o2_cc_pvtz_cart_num():
@@ -105,91 +109,89 @@ def test_load_fchk_o2_cc_pvtz_cart_num():
     coordinates = fields['coordinates']
     numbers = fields['numbers']
     energy = fields['energy']
-    assert len(obasis["shell_types"]) == 20
-    assert shells_to_nbasis(obasis["shell_types"]) == 70
-    assert len(coordinates) == len(numbers)
-    assert coordinates.shape[1] == 3
-    assert len(numbers) == 2
-    assert energy == -1.495953594545721E+02
+    assert_allclose(len(obasis["shell_types"]), 20)
+    assert_allclose(shells_to_nbasis(obasis["shell_types"]), 70)
+    assert_allclose(len(coordinates), len(numbers))
+    assert_allclose(coordinates.shape[1], 3)
+    assert_allclose(len(numbers), 2)
+    assert_allclose(energy, -1.495953594545721E+02)
 
 
 def test_load_fchk_water_sto3g_hf():
     with path('iodata.test.data', 'water_sto3g_hf_g03.fchk') as fn:
         fields = load(str(fn))
     obasis = fields['obasis']
-    assert len(obasis["shell_types"]) == 5
-    assert shells_to_nbasis(obasis["shell_types"]) == 7
+    assert_equal(len(obasis["shell_types"]), 5)
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 7)
     coordinates = fields['coordinates']
     numbers = fields['numbers']
-    assert len(coordinates) == len(numbers)
-    assert coordinates.shape[1] == 3
-    assert len(numbers) == 3
+    assert_equal(len(coordinates), len(numbers))
+    assert_equal(coordinates.shape[1], 3)
+    assert_equal(len(numbers), 3)
     orb_alpha = fields['orb_alpha']
     orb_alpha_coeffs = fields['orb_alpha_coeffs']
     orb_alpha_energies = fields['orb_alpha_energies']
     orb_alpha_occs = fields['orb_alpha_occs']
     assert orb_alpha[0] == 7
-    assert abs(orb_alpha_energies[0] - (-2.02333942E+01)) < 1e-7
-    assert abs(orb_alpha_energies[-1] - 7.66134805E-01) < 1e-7
-    assert abs(orb_alpha_coeffs[0, 0] - 0.99410) < 1e-4
-    assert abs(orb_alpha_coeffs[1, 0] - 0.02678) < 1e-4
-    assert abs(orb_alpha_coeffs[-1, 2] - (-0.44154)) < 1e-4
+    assert_allclose(orb_alpha_energies[0], (-2.02333942E+01), atol=1.e-7)
+    assert_allclose(orb_alpha_energies[-1], 7.66134805E-01, atol=1.e-7)
+    assert_allclose(orb_alpha_coeffs[0, 0], 0.99410, atol=1.e-4)
+    assert_allclose(orb_alpha_coeffs[1, 0], 0.02678, atol=1.e-4)
+    assert_allclose(orb_alpha_coeffs[-1, 2], -0.44154, atol=1.e-4)
     assert abs(orb_alpha_coeffs[3, -1]) < 1e-4
-    assert abs(orb_alpha_coeffs[4, -1] - (-0.82381)) < 1e-4
-    assert abs(orb_alpha_occs.sum() - 5) == 0.0
-    assert orb_alpha_occs.min() == 0.0
-    assert orb_alpha_occs.max() == 1.0
+    assert_allclose(orb_alpha_coeffs[4, -1], -0.82381, atol=1.e-4)
+    assert_equal(orb_alpha_occs.sum(), 5)
+    assert_equal(orb_alpha_occs.min(), 0.0)
+    assert_equal(orb_alpha_occs.max(), 1.0)
     energy = fields['energy']
-    assert energy == -7.495929232844363E+01
+    assert_allclose(energy, -7.495929232844363E+01)
 
 
 def test_load_fchk_lih_321g_hf():
     with path('iodata.test.data', 'li_h_3-21G_hf_g09.fchk') as fn:
         fields = load(str(fn))
     obasis = fields['obasis']
-    assert len(obasis["shell_types"]) == 7
-    assert shells_to_nbasis(obasis["shell_types"]) == 11
+    assert_equal(len(obasis["shell_types"]), 7)
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 11)
     coordinates = fields['coordinates']
     numbers = fields['numbers']
-    assert len(coordinates) == len(numbers)
-    assert coordinates.shape[1] == 3
-    assert len(numbers) == 2
+    assert_equal(len(coordinates), len(numbers))
+    assert_equal(coordinates.shape[1], 3)
+    assert_equal(len(numbers), 2)
     orb_alpha = fields['orb_alpha']
     orb_alpha_coeffs = fields['orb_alpha_coeffs']
     orb_alpha_energies = fields['orb_alpha_energies']
     orb_alpha_occs = fields['orb_alpha_occs']
-    assert orb_alpha[0] == 11
-    assert abs(orb_alpha_energies[0] - (-2.76117)) < 1e-4
-    assert abs(orb_alpha_energies[-1] - 0.97089) < 1e-4
-    assert abs(orb_alpha_coeffs[0, 0] - 0.99105) < 1e-4
-    assert abs(orb_alpha_coeffs[1, 0] - 0.06311) < 1e-4
-    assert abs(orb_alpha_coeffs[3, 2]) < 1e-4
-    assert abs(orb_alpha_coeffs[-1, 9] - 0.13666) < 1e-4
-    assert abs(orb_alpha_coeffs[4, -1] - 0.17828) < 1e-4
-    assert abs(orb_alpha_occs.sum() - 2) == 0.0
-    assert orb_alpha_occs.min() == 0.0
-    assert orb_alpha_occs.max() == 1.0
+    assert_equal(orb_alpha[0], 11)
+    assert_allclose(orb_alpha_energies[0], (-2.76117), atol=1.e-4)
+    assert_allclose(orb_alpha_energies[-1], 0.97089, atol=1.e-4)
+    assert_allclose(orb_alpha_coeffs[0, 0], 0.99105, atol=1.e-4)
+    assert_allclose(orb_alpha_coeffs[1, 0], 0.06311, atol=1.e-4)
+    assert orb_alpha_coeffs[3, 2] < 1.e-4
+    assert_allclose(orb_alpha_coeffs[-1, 9], 0.13666, atol=1.e-4)
+    assert_allclose(orb_alpha_coeffs[4, -1], 0.17828, atol=1.e-4)
+    assert_equal(orb_alpha_occs.sum(), 2)
+    assert_equal(orb_alpha_occs.min(), 0.0)
+    assert_equal(orb_alpha_occs.max(), 1.0)
     orb_beta = fields['orb_beta']
     orb_beta_coeffs = fields['orb_beta_coeffs']
     orb_beta_energies = fields['orb_beta_energies']
     orb_beta_occs = fields['orb_beta_occs']
-    assert orb_beta[0] == 11
-    assert abs(orb_beta_energies[0] - (-2.76031)) < 1e-4
-    assert abs(orb_beta_energies[-1] - 1.13197) < 1e-4
-    assert abs(orb_beta_coeffs[0, 0] - 0.99108) < 1e-4
-    assert abs(orb_beta_coeffs[1, 0] - 0.06295) < 1e-4
+    assert_equal(orb_beta[0], 11)
+    assert_allclose(orb_beta_energies[0], -2.76031, atol=1.e-4)
+    assert_allclose(orb_beta_energies[-1], 1.13197, atol=1.e-4)
+    assert_allclose(orb_beta_coeffs[0, 0], 0.99108, atol=1.e-4)
+    assert_allclose(orb_beta_coeffs[1, 0], 0.06295, atol=1.e-4)
     assert abs(orb_beta_coeffs[3, 2]) < 1e-4
-    assert abs(orb_beta_coeffs[-1, 9] - 0.80875) < 1e-4
-    assert abs(orb_beta_coeffs[4, -1] - (-0.15503)) < 1e-4
-    assert abs(orb_beta_occs.sum() - 1) == 0.0
-    assert orb_beta_occs.min() == 0.0
-    assert orb_beta_occs.max() == 1.0
-
-    assert orb_alpha_occs.shape[0] == orb_alpha_coeffs.shape[0]
-    assert orb_beta_occs.shape[0] == orb_beta_coeffs.shape[0]
-
+    assert_allclose(orb_beta_coeffs[-1, 9], 0.80875, atol=1.e-4)
+    assert_allclose(orb_beta_coeffs[4, -1], -0.15503, atol=1.e-4)
+    assert_equal(orb_beta_occs.sum(), 1)
+    assert_equal(orb_beta_occs.min(), 0.0)
+    assert_equal(orb_beta_occs.max(), 1.0)
+    assert_equal(orb_alpha_occs.shape[0], orb_alpha_coeffs.shape[0])
+    assert_equal(orb_beta_occs.shape[0], orb_beta_coeffs.shape[0])
     energy = fields['energy']
-    assert energy == -7.687331212191968E+00
+    assert_allclose(energy, -7.687331212191968E+00)
 
 
 def test_load_fchk_ghost_atoms():
@@ -203,10 +205,10 @@ def test_load_fchk_ghost_atoms():
     # There should be 3 real atoms and 3 ghost atoms
     natom = 3
     nghost = 3
-    assert numbers.shape[0] == natom
-    assert coordinates.shape[0] == natom
-    assert mulliken_charges.shape[0] == natom
-    assert obasis['centers'].shape[0] == (natom + nghost)
+    assert_equal(numbers.shape[0], natom)
+    assert_equal(coordinates.shape[0], natom)
+    assert_equal(mulliken_charges.shape[0], natom)
+    assert_equal(obasis['centers'].shape[0], natom + nghost)
 
 
 def test_load_fchk_ch3_rohf_g03():
@@ -217,12 +219,12 @@ def test_load_fchk_ch3_rohf_g03():
     orb_alpha_occs = fields['orb_alpha_occs']
     orb_beta_coeffs = fields['orb_beta_coeffs']
     orb_beta_occs = fields['orb_beta_occs']
-    assert orb_alpha_occs.shape[0] == orb_alpha_coeffs.shape[0]
-    assert orb_beta_occs.shape[0] == orb_beta_coeffs.shape[0]
-    assert orb_alpha_occs.sum() == 5
+    assert_equal(orb_alpha_occs.shape[0], orb_alpha_coeffs.shape[0])
+    assert_equal(orb_beta_occs.shape[0], orb_beta_coeffs.shape[0])
+    assert_equal(orb_alpha_occs.sum(), 5)
     orb_beta = fields['orb_beta']
-    assert orb_beta_occs.sum() == 4
-    assert (orb_alpha_coeffs == orb_beta_coeffs).all()
+    assert_equal(orb_beta_occs.sum(), 4)
+    assert_allclose(orb_alpha_coeffs, orb_beta_coeffs)
     assert not (orb_alpha is orb_beta)
     assert 'dm_full_scf' not in fields
 
@@ -231,10 +233,10 @@ def check_load_azirine(key, numbers):
     with path('iodata.test.data', '2h-azirine-{}.fchk'.format(key)) as fn:
         fields = load(str(fn))
     obasis = fields['obasis']
-    assert shells_to_nbasis(obasis["shell_types"]) == 33
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 33)
     dm_full = fields['dm_full_%s' % key]
-    assert dm_full[0, 0] == numbers[0]
-    assert dm_full[32, 32] == numbers[1]
+    assert_equal(dm_full[0, 0], numbers[0])
+    assert_equal(dm_full[32, 32], numbers[1])
 
 
 def test_load_azirine_cc():
@@ -257,29 +259,33 @@ def check_load_nitrogen(key, numbers_full, numbers_spin):
     with path('iodata.test.data', 'nitrogen-{}.fchk'.format(key)) as fn:
         fields = load(str(fn))
     obasis = fields['obasis']
-    assert shells_to_nbasis(obasis["shell_types"]) == 9
+    assert_equal(shells_to_nbasis(obasis["shell_types"]), 9)
     dm_full = fields['dm_full_%s' % key]
-    assert dm_full[0, 0] == numbers_full[0]
-    assert dm_full[8, 8] == numbers_full[1]
+    assert_equal(dm_full[0, 0], numbers_full[0])
+    assert_equal(dm_full[8, 8], numbers_full[1])
     dm_spin = fields['dm_spin_%s' % key]
-    assert dm_spin[0, 0] == numbers_spin[0]
-    assert dm_spin[8, 8] == numbers_spin[1]
+    assert_equal(dm_spin[0, 0], numbers_spin[0])
+    assert_equal(dm_spin[8, 8], numbers_spin[1])
 
 
 def test_load_nitrogen_cc():
-    check_load_nitrogen('cc', [2.08709209E+00, 3.74723580E-01], [7.25882619E-04, -1.38368575E-02])
+    check_load_nitrogen('cc', [2.08709209E+00, 3.74723580E-01],
+                        [7.25882619E-04, -1.38368575E-02])
 
 
 def test_load_nitrogen_ci():
-    check_load_nitrogen('ci', [2.08741410E+00, 2.09292886E-01], [7.41998558E-04, -6.67582215E-03])
+    check_load_nitrogen('ci', [2.08741410E+00, 2.09292886E-01],
+                        [7.41998558E-04, -6.67582215E-03])
 
 
 def test_load_nitrogen_mp2():
-    check_load_nitrogen('mp2', [2.08710027E+00, 4.86472609E-01], [7.31802950E-04, -2.00028488E-02])
+    check_load_nitrogen('mp2', [2.08710027E+00, 4.86472609E-01],
+                        [7.31802950E-04, -2.00028488E-02])
 
 
 def test_load_nitrogen_mp3():
-    check_load_nitrogen('mp3', [2.08674302E+00, 4.91149023E-01], [7.06941101E-04, -1.96276763E-02])
+    check_load_nitrogen('mp3', [2.08674302E+00, 4.91149023E-01],
+                        [7.06941101E-04, -1.96276763E-02])
 
 
 def check_normalization_dm_full_azirine(key):
@@ -289,7 +295,7 @@ def check_normalization_dm_full_azirine(key):
     olp = compute_overlap(**mol.obasis)
     dm = getattr(mol, 'dm_full_%s' % key)
     check_dm(dm, olp, eps=1e-2, occ_max=2)
-    assert abs(np.einsum('ab,ba', olp, dm) - 22.0) < 1e-3
+    assert_allclose(np.einsum('ab,ba', olp, dm), 22.0, atol=1.e-3)
 
 
 def test_normalization_dm_full_azirine_cc():
@@ -311,29 +317,29 @@ def test_normalization_dm_full_azirine_mp3():
 def test_load_water_hfs_321g():
     with path('iodata.test.data', 'water_hfs_321g.fchk') as fn:
         mol = IOData.from_file(str(fn))
-    assert mol.polar[0, 0] == 7.23806684E+00
-    assert mol.polar[1, 1] == 8.04213953E+00
-    assert mol.polar[1, 2] == 1.20021770E-10
-    np.testing.assert_allclose(mol.dipole_moment, [
-        -5.82654324E-17, 0.00000000E+00, -8.60777067E-01])
-    np.testing.assert_allclose(mol.quadrupole_moment, [
-        -8.89536026E-01,  # xx
-        8.28408371E-17,  # xy
-        4.89353090E-17,  # xz
-        1.14114241E+00,  # yy
-        -5.47382213E-48,  # yz
-        -2.51606382E-01])  # zz
+    assert_allclose(mol.polar[0, 0], 7.23806684E+00)
+    assert_allclose(mol.polar[1, 1], 8.04213953E+00)
+    assert_allclose(mol.polar[1, 2], 1.20021770E-10)
+    assert_allclose(mol.dipole_moment,
+                    [-5.82654324E-17, 0.00000000E+00, -8.60777067E-01])
+    assert_allclose(mol.quadrupole_moment,
+                    [-8.89536026E-01,  # xx
+                     8.28408371E-17,  # xy
+                     4.89353090E-17,  # xz
+                     1.14114241E+00,  # yy
+                     -5.47382213E-48,  # yz
+                     -2.51606382E-01])  # zz
 
 
 def test_load_monosilicic_acid_hf_lan():
     with path('iodata.test.data', 'monosilicic_acid_hf_lan.fchk') as fn:
         mol = IOData.from_file(str(fn))
-    np.testing.assert_allclose(mol.dipole_moment, [
-        -6.05823053E-01, -9.39656399E-03, 4.18948869E-01])
-    np.testing.assert_allclose(mol.quadrupole_moment, [
-        2.73609152E+00,  # xx
-        -6.65787832E-02,  # xy
-        2.11973730E-01,  # xz
-        8.97029351E-01,  # yy
-        -1.38159653E-02,  # yz
-        -3.63312087E+00])  # zz
+    assert_allclose(mol.dipole_moment,
+                    [-6.05823053E-01, -9.39656399E-03, 4.18948869E-01])
+    assert_allclose(mol.quadrupole_moment,
+                    [2.73609152E+00,  # xx
+                     -6.65787832E-02,  # xy
+                     2.11973730E-01,  # xz
+                     8.97029351E-01,  # yy
+                     -1.38159653E-02,  # yz
+                     -3.63312087E+00])  # zz
