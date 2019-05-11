caae410a8d4a24dea9181a29ad5e510d3bb14cd1
Toon Verstraelen <toon.verstraelen@UGent.be>
2019-05-11 20:21:05 +0200

    Fixup

--- iodata/test/test_gaussianlog.py
+++ iodata/test/test_gaussianlog.py
@@ -16,12 +16,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 # --
+# pylint: disable=no-member
 """Test iodata.formats.log module."""
 
 from numpy.testing import assert_equal, assert_allclose
 
 from ..iodata import load_one
-from ..utils import LineIterator
 
 try:
     from importlib_resources import path
