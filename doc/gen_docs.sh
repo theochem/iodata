7e46078d99648c0a235e5c82f0aaccf8edc8a3cb
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2019-05-13 19:32:39 +0200

    Clean up of doc build

---
+++ doc/gen_docs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+SPHINX_APIDOC_OPTIONS=members,undoc-members,show-inheritance,inherited-members sphinx-apidoc -o pyapi/ ../iodata/ ../iodata/test/test_*.py ../iodata/test/cached --separate
