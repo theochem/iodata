b7221401e6baad7bfe004fa5bbae57298d0e7d79
Fanwang Meng <fwmeng88@gmail.com>
2019-05-14 02:37:04 -0400

    Add Avogadro constant and calorie

--- iodata/utils.py
+++ iodata/utils.py
@@ -38,7 +38,10 @@ angstrom: float = spc.angstrom / spc.value(u'atomic unit of length')
 electronvolt: float = 1 / spc.value(u'hartree-electron volt relationship')
 # atomic mass unit (not atomic unit of mass!)
 amu: float = 1e-3 / (spc.value(u'electron mass') * spc.value(u'Avogadro constant'))
-
+# Avogadro constant
+avogadro = spc.value('Avogadro constant')
+# calorie
+calorie = spc.calorie
 
 class FileFormatError(IOError):
     """Raised when incorrect content is encountered when loading files."""
