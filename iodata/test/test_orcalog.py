efae4bb1f35dca071d4b6c41d76730e0882643de
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2020-06-19 09:10:48 -0700

    Rename formats/orca.py to formats/orcalog.py
    The module name was changed to better reflect the fact that it parses
    ORCA output file, similar to gaussianlog.py.

--- iodata/test/test_orcalog.py
+++ iodata/test/test_orcalog.py
@@ -17,7 +17,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 # --
 # pylint: disable=unsubscriptable-object
-"""Test iodata.formats.orca module."""
+"""Test iodata.formats.orcalog module."""
 
 import numpy as np
 from numpy.testing import assert_equal, assert_allclose
