1f8ef8f581b796a50ad9ba412bc929344b5f41d8
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2020-06-19 09:23:48 -0700

    Rename formats/molpro.py to formats/fcidump.py
    The module name was changed to better reflect the fact that it parses
    FCIDUMP files.

--- iodata/test/test_fcidump.py
+++ iodata/test/test_fcidump.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 # --
-"""Test iodata.formats.molpro module."""
+"""Test iodata.formats.fcidump module."""
 
 import os
 
