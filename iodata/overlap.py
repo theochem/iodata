1f7d726b0ef736cf46aada8353784e4ac4f8d79c
BradenDKelly <bradenzkelly@gmail.com>
2020-07-14 05:20:07 -0400

    Vectorized using np.frompyfunc and has prefactors taken out of loops

--- iodata/overlap.py
+++ iodata/overlap.py
@@ -18,7 +18,6 @@
 # --
 """Module for computing overlap of atomic orbital basis functions."""
 
-
 import numpy as np
 from scipy.special import binom, factorial2
 
@@ -26,8 +25,7 @@ from .overlap_cartpure import tfs
 from .basis import convert_conventions, iter_cart_alphabet, MolecularBasis
 from .basis import HORTON2_CONVENTIONS as OVERLAP_CONVENTIONS
 
-
-__all__ = ['OVERLAP_CONVENTIONS', 'compute_overlap_py', 'gob_cart_normalization']
+__all__ = ['OVERLAP_CONVENTIONS', 'compute_overlap', 'gob_cart_normalization']
 
 
 def compute_overlap(obasis: MolecularBasis, atcoords: np.ndarray) -> np.ndarray:
@@ -69,9 +67,11 @@ def compute_overlap(obasis: MolecularBasis, atcoords: np.ndarray) -> np.ndarray:
     n_max = np.max([np.max(shell.angmoms) for shell in obasis.shells])
     go = GaussianOverlap(n_max)
 
+    # define a python ufunc (numpy function) for broadcasted calling over angular momentums
+    compute_overlap_1d = np.frompyfunc(go.compute_overlap_gaussian_1d, 5, 1)
+
     # Loop over shell0
     begin0 = 0
-    count = 0
     total = 0
     for i0, shell0 in enumerate(obasis.shells):
         r0 = atcoords[shell0.icenter]
@@ -88,27 +88,42 @@ def compute_overlap(obasis: MolecularBasis, atcoords: np.ndarray) -> np.ndarray:
 
             a0 = np.min(shell0.exponents)
             a1 = np.min(shell1.exponents)
-            prefactor = np.exp(-a0 * a1 * np.linalg.norm(r0 - r1) ** 2 / (a0 + a1))
+
+            # prepare some constants to save FLOPS later on
+            rij = r0 - r1
+            rij_norm_sq = np.linalg.norm(rij) ** 2
+            prefactor = np.exp(-a0 * a1 * rij_norm_sq / (a0 + a1))
             total += 1
             if prefactor > 1.e-15:
-                count += 1
+                # arrays of angular momentums [[2, 0, 0], [0, 2, 0], ..., [0, 1, 1]]
+                n0 = np.array(list(iter_cart_alphabet(shell0.angmoms[0])))
+                n1 = np.array(list(iter_cart_alphabet(shell1.angmoms[0])))
+
                 # Loop over primitives in shell0 (Cartesian)
                 for iexp0, (a0, cc0) in enumerate(zip(shell0.exponents, shell0.coeffs[:, 0])):
                     scales0 = scales[i0][iexp0]
+                    a0_r0 = a0 * r0
 
                     # Loop over primitives in shell1 (Cartesian)
                     for iexp1, (a1, cc1) in enumerate(zip(shell1.exponents, shell1.coeffs[:, 0])):
                         scales1 = scales[i1][iexp1]
 
-                        prefactor = np.exp(-a0 * a1 * np.linalg.norm(r0 - r1) ** 2 / (a0 + a1))
+                        at = a0 + a1
+                        prefactor = np.exp(-a0 * a1 / at * rij_norm_sq)
                         if prefactor < 1.0e-15:
                             continue
+                        # prepare some pre-factors to save FLOPS in inner loop
+                        two_at = 2 * at
+                        prefactor *= (np.pi / at) ** (3 / 2)
+                        rn = (a0_r0 + a1 * r1) / at
+                        rn_0 = rn - r0
+                        rn_1 = rn - r1
 
-                        for s0, n0, in enumerate(iter_cart_alphabet(shell0.angmoms[0])):
-                            for s1, n1, in enumerate(iter_cart_alphabet(shell1.angmoms[0])):
-                                v = go.compute_overlap_gaussian_3d(r0, r1, a0, a1, n0, n1)
-                                v *= cc0 * cc1 * scales0[s0] * scales1[s1]
-                                result[s0, s1] += v
+                        v = np.prod(compute_overlap_1d(rn_0, rn_1, n0[:, None, :], n1[None, :, :],
+                                                       two_at), axis=2)
+                        v *= prefactor
+                        result = np.add(result, v * cc0 * cc1 * scales0[:, None] * scales1[None, :])
+
             # END of Cartesian coordinate system (if going to pure coordinates)
 
             # cart to pure
@@ -128,7 +143,6 @@ def compute_overlap(obasis: MolecularBasis, atcoords: np.ndarray) -> np.ndarray:
     permutation, signs = convert_conventions(obasis, OVERLAP_CONVENTIONS, reverse=True)
     overlap = overlap[permutation] * signs.reshape(-1, 1)
     overlap = overlap[:, permutation] * signs
-    print("count = ", count, total)
     return overlap
 
 
@@ -139,32 +153,17 @@ class GaussianOverlap():
         facts.insert(0, 1)
         self.facts = np.array(facts)
 
-    def compute_overlap_gaussian_3d(self, r1, r2, a1, a2, n1, n2):
-        """Compute overlap integral of two Gaussian functions in three-dimensions."""
-        value = self.compute_overlap_gaussian_1d(r1[0], r2[0], a1, a2, n1[0], n2[0])
-        value *= self.compute_overlap_gaussian_1d(r1[1], r2[1], a1, a2, n1[1], n2[1])
-        value *= self.compute_overlap_gaussian_1d(r1[2], r2[2], a1, a2, n1[2], n2[2])
-        return value
-
-    def compute_overlap_gaussian_1d(self, x1, x2, a1, a2, n1, n2):
+    def compute_overlap_gaussian_1d(self, x1, x2, n1, n2, two_at):
         """Compute overlap integral of two Gaussian functions in one-dimensions."""
-        # compute total exponent and new x
-        at = a1 + a2
-        xn = (a1 * x1 + a2 * x2) / at
-        pf = np.exp(-a1 * a2 * (x1 - x2) ** 2 / at)
-        x1 = xn - x1
-        x2 = xn - x2
         # compute overlap
         value = 0
-
         for i in range(n1 + 1):
             pf_i = self.binomials[n1][i] * x1 ** (n1 - i)
             for j in range(n2 + 1):
                 m = i + j
                 if m % 2 == 0:
-                    integ = self.facts[i + j - 1 + 1] / (2 * at) ** (m / 2)
+                    integ = self.facts[m] / two_at ** (m / 2)
                     value += pf_i * self.binomials[n2][j] * x2 ** (n2 - j) * integ
-        value *= pf * np.sqrt(np.pi / at)
         return value
 
 
@@ -210,6 +209,5 @@ def gob_cart_normalization(alpha: np.ndarray, n: np.ndarray) -> np.ndarray:
         The normalization constant for the gaussian cartesian basis.
 
     """
-    vfac2 = np.vectorize(factorial2)
-    return np.sqrt((4 * alpha)**sum(n) * (2 * alpha / np.pi)**1.5
-                   / np.prod(vfac2(2 * n - 1)))
+    return np.sqrt((4 * alpha) ** sum(n) * (2 * alpha / np.pi) ** 1.5
+                   / np.prod(factorial2(2 * n - 1)))
