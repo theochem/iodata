7224152008ed9f49bdfe79f0f2a34eec37d00638
Luis Macaya <lmacaya@udec.cl>
2020-08-02 14:38:31 -0400

    Remove try except from _helper_read_frame

--- iodata/formats/gromacs.py
+++ iodata/formats/gromacs.py
@@ -99,14 +99,7 @@ def _helper_read_frame(lit: LineIterator) -> Tuple:
     if 't=' in line:
         time = float(line.split('t=')[1]) * picosecond
     # Read the second line for number of atoms.
-    natoms = next(lit)
-    # Just a checkpoint. This line must be a number.
-    # If is not, the data is not parsed.
-    if natoms is not None:
-        try:
-            natoms = int(natoms)
-        except ValueError:
-            return None
+    natoms = int(next(lit))
     # Read the atom lines
     resnums = []
     resnames = []
