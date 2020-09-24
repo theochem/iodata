818fd56d2d169017cfca97c0a6d7c2c25eff6b2e
William Adams <wil.h.adams@gmail.com>
2020-09-24 14:52:52 -0400

    Add QCSchema Molecule load_one
    qcschema_molecule is a representation of a molecular system containing all data
    necessary to specify a molecule and support I/O and manipulation processes. This
    implementation of qcschema_molecule is based on the currently available
    specification on the QCSchema GitHub repo. Due to the QCSchema's dependence on
    QCElemental, which firstly uses Pydantic to implement the QCSchema as its internal
    data structure and secondly generates a JSON schema based on that implementation,
    the specification could be subject to change without notice.

--- iodata/formats/json.py
+++ iodata/formats/json.py
@@ -19,18 +19,20 @@
 """JSON file format."""
 
 
-from typing import List, Tuple, Union, TextIO
 import json
+from typing import List, Union
+from warnings import warn
 
 import numpy as np
 
-from ..basis import CCA_CONVENTIONS, Shell, MolecularBasis
-from ..docstrings import document_load_one, document_dump_one
-from ..iodata import IOData
-from ..periodic import sym2num, num2sym
-from ..orbitals import MolecularOrbitals
-from ..utils import angstrom, FileFormatError, FileFormatWarning, LineIterator
-from warnings import warn
+from ..docstrings import document_load_one
+from ..periodic import sym2num
+from ..utils import FileFormatError, FileFormatWarning, LineIterator
+
+try:
+    from iodata.version import __version__
+except ImportError:
+    __version__ = "0.0.0.post0"
 
 __all__ = []
 
@@ -38,7 +40,8 @@ __all__ = []
 PATTERNS = ["*.json"]
 
 
-@document_load_one("JSON", ["atcoords", "atnums", "atcorenums", "mo", "obasis"], ["title"])
+# FIXME: check this
+@document_load_one("QCSchema", ["atcoords", "atnums", "atcorenums", "mo", "obasis"], ["title"])
 def load_one(lit: LineIterator) -> dict:
     """Do not edit this docstring. It will be overwritten."""
     # Use python standard lib json module to read the file to a dict
@@ -89,7 +92,7 @@ def _parse_json(json_in: dict, lit: LineIterator) -> dict:
             "qcschema_molecule",
             "qcschema_basis",
             "qcschema_input",
-            "qcschema_output"
+            "qcschema_output",
         }:
             del result["schema_name"]
     if "schema_name" not in result:
@@ -131,12 +134,11 @@ def _parse_json(json_in: dict, lit: LineIterator) -> dict:
     if schema_name == "qcschema_molecule":
         return _load_qcschema_molecule(result, lit)
     elif schema_name == "qcschema_basis":
-        # FIXME add _load_qcschema_basis
-        return _load_qcschema_basis(result, lit)
+        raise NotImplementedError("{} is not yet implemented in IOData.".format(schema_name))
     elif schema_name == "qcschema_input":
-        return _load_qcschema_input(result, lit)
+        raise NotImplementedError("{} is not yet implemented in IOData.".format(schema_name))
     elif schema_name == "qcschema_output":
-        return _load_qcschema_output(result, lit)
+        raise NotImplementedError("{} is not yet implemented in IOData.".format(schema_name))
     else:
         raise FileFormatError(
             "{}: Invalid QCSchema type {}, should be one of `qcschema_molecule`, `qcschema_basis`,"
@@ -163,9 +165,218 @@ def _load_qcschema_molecule(result: dict, lit: LineIterator) -> dict:
         and corresponding values as well.
 
     """
-    molecule_dict = dict()
+    # All Topology properties are found in the "molecule" key
+    molecule_dict = _parse_topology_keys(result, lit)
 
     return molecule_dict
+
+
+def _parse_topology_keys(mol: dict, lit: LineIterator) -> dict:
+    """Load topology properties from old QCSchema Molecule specifications.
+
+    The qcschema_molecule v2 specification requires a topology for every file, specified in the
+    `molecule` key, containing at least the keys `schema_name`, `schema_version`, `symbols`,
+    `geometry`, `molecular_charge`, `molecular_multiplicity`, and `provenance`. This schema is
+    currently used in QCElemental (and thus the QCArchive ecosystem).
+
+    qcschema_molecule v1 only exists as the specification on the QCSchema website, and seems never
+    to have been implemented in QCArchive. It is possible to accept v1 input, since all required
+    keys for v2 exist as keys in v1, but it is preferable to convert those files to v2 explicitly.
+
+    Parameters
+    ----------
+    mol
+        The 'molecule' key from the QCSchema file.
+    lit
+        The line iterator holding the file data.
+
+    Returns
+    -------
+    topology_dict
+        Output dictionary containing ``atcoords`` & ``atnums`` keys and corresponding values.
+        It may contain ``atcorenums``, ``atmasses``, ``bonds``, ``charge`` & ``extra`` keys
+        and corresponding values as well.
+
+    """
+    # Make sure required topology properties are present
+    # NOTE: currently molecular_charge and molecular_multiplicity have default values,
+    # so some programs may not provide those required keys
+    # QCEngineRecords files follow Molecule v1 for some reason, despite being uploaded in 2019
+    should_be_required_keys = {"schema_name", "schema_version", "provenance"}
+    topology_keys = {
+        "symbols",
+        "geometry",
+    }
+    for key in should_be_required_keys:
+        if key not in mol:
+            warn(
+                "{}: QCSchema files should have a '{}' key.".format(lit.filename, key),
+                FileFormatWarning,
+                2,
+            )
+    for key in topology_keys:
+        if key not in mol:
+            raise FileFormatError(
+                "{}: QCSchema topology requires '{}' key".format(lit.filename, key)
+            )
+
+    topology_dict = dict()
+    extra_dict = dict()
+
+    # Save schema name & version
+    extra_dict["schema_name"] = "qcschema_molecule"
+    try:
+        version = mol["schema_version"]
+    except KeyError:
+        version = -1
+    if float(version) < 0 or float(version) > 2:
+        warn(
+            "{}: Unknown `qcschema_molecule` version {}, "
+            "loading may produce invalid results".format(lit.filename, version),
+            FileFormatWarning,
+            2,
+        )
+    extra_dict["schema_version"] = version
+
+    atnums = np.array([sym2num[symbol.title()] for symbol in mol["symbols"]])
+    atcorenums = atnums.copy()
+    topology_dict["atnums"] = atnums
+    # Geometry is in a flattened list, convert to N x 3
+    topology_dict["atcoords"] = np.array(
+        [mol["geometry"][3 * i : (3 * i) + 3] for i in range(0, len(mol["geometry"]) // 3)]
+    )
+    # Check for missing charge, warn that this is a required field
+    if "molecular_charge" not in mol:
+        warn(
+            "{}: Missing 'molecular_charge' key."
+            "Some QCSchema writers omit this key for default value 0.0,"
+            "Ensure this value is correct.",
+            FileFormatWarning,
+            2,
+        )
+        formal_charge = 0.0
+    else:
+        formal_charge = mol["molecular_charge"]
+    # Determine number of electrons from atomic numbers and formal charge
+    topology_dict["charge"] = formal_charge
+    topology_dict["nelec"] = np.sum(atnums) - formal_charge
+    # Check for missing mult, warn that this is a required field
+    if "molecular_multiplicity" not in mol:
+        warn(
+            "{}: Missing 'molecular_multiplicity' key."
+            "Some QCSchema writers omit this key for default value 1,"
+            "Ensure this value is correct.",
+            FileFormatWarning,
+            2,
+        )
+        topology_dict["spinpol"] = 0
+    else:
+        mult = mol["molecular_multiplicity"]
+        topology_dict["spinpol"] = mult - 1
+
+    # Provenance is optional in v1
+    if "provenance" in mol:
+        extra_dict["provenance"] = _parse_provenance(
+            mol["provenance"], lit, "qcschema_molecule", False
+        )
+
+    # Check for optional keys
+    # Load ghost atoms as atoms with zero effective core charge
+    if "real" in mol:
+        ghosts = mol["real"]
+        extra_dict["real"] = ghosts
+        atcorenums[ghosts is False] = 0
+    # Load atom masses to array, canonical weights assumed if masses not given
+    if "masses" in mol and "mass_numbers" in mol:
+        warn(
+            "{}: Both `masses` and `mass_numbers` given. "
+            "Both values will be written to `extra` dict.",
+            FileFormatWarning,
+            2,
+        )
+        extra_dict["mass_numbers"] = np.array(mol["mass_numbers"])
+        extra_dict["masses"] = np.array(mol["masses"])
+    elif "masses" in mol:
+        topology_dict["atmasses"] = np.array(mol["masses"])
+    elif "mass_numbers" in mol:
+        topology_dict["atmasses"] = np.array(mol["mass_numbers"])
+    # Load bonds: list of tuple (atom1, atom2, bond_order)
+    # Note: The QCSchema spec allows for non-integer bond_orders, these are forced to integers here
+    # in accordance with current IOData specification
+    if "connectivity" in mol:
+        topology_dict["bonds"] = np.array(mol["connectivity"], dtype=int)
+    # Check for fragment keys
+    # List fragment indices in nested list (likely is a jagged array)
+    if "fragments" in mol:
+        fragments = mol["fragments"]
+        extra_dict["fragments"] = {"indices": fragments}
+        if "fragment_charges" in mol:
+            extra_dict["fragments"]["charges"] = mol["fragment_charges"]
+        if "fragment_multiplicities" in mol:
+            extra_dict["fragments"]["multiplicities"] = mol["fragment_multiplicities"]
+    if "fix_symmetry" in mol:
+        topology_dict["g_rot"] = mol["fix_symmetry"]
+    if "fix_orientation" in mol:
+        extra_dict["fix_orientation"] = mol["fix_orientation"]
+
+    # Check for other extras
+    # name, comment, fix_com, fix_symmetry, fix_orientation
+    if "name" in mol:
+        topology_dict["title"] = mol["name"]
+    if "comment" in mol:
+        extra_dict["comment"] = mol["comment"]
+    if "fix_com" in mol:
+        extra_dict["fix_com"] = mol["fix_com"]
+    if "identifiers" in mol:
+        extra_dict["identifiers"] = mol["identifiers"]
+    if "validated" in mol:
+        extra_dict["qcel_validated"] = mol["validated"]
+    if "atom_labels" in mol:
+        extra_dict["atom_labels"] = mol["atom_labels"]
+    if "atomic_numbers" in mol:
+        extra_dict["atomic_numbers"] = mol["atomic_numbers"]
+    if "id" in mol:
+        extra_dict["id"] = mol["id"]
+    if "extras" in mol:
+        extra_dict["extras"] = mol["extras"]
+
+    if extra_dict:
+        topology_dict["extra"] = extra_dict
+
+    molecule_keys = {
+        "schema_name",
+        "schema_version",
+        "validated",
+        "symbols",
+        "geometry",
+        "name",
+        "identifiers",
+        "comment",
+        "molecular_charge",
+        "molecular_multiplicity",
+        "masses",
+        "real",
+        "atom_labels",
+        "atomic_numbers",
+        "mass_numbers",
+        "connectivity",
+        "fragments",
+        "fragment_charges",
+        "fragment_multiplicities",
+        "fix_com",
+        "fix_orientation",
+        "fix_symmetry",
+        "provenance",
+        "id",
+        "extras",
+    }
+    parsed_keys = molecule_keys.intersection(mol.keys())
+    for key in parsed_keys:
+        del mol[key]
+    if mol:
+        topology_dict["extra"]["unparsed"] = mol
+
+    return topology_dict
 
 
 def _load_qcschema_basis(result: dict, lit: LineIterator) -> dict:
@@ -226,3 +437,45 @@ def _load_qcschema_output(result: dict, lit: LineIterator) -> dict:
     output_dict = dict()
 
     return output_dict
+
+
+def _parse_provenance(
+    provenance: Union[List[dict], dict], lit: LineIterator, source: str, append=True
+) -> Union[List[dict], dict]:
+    """Load provenance properties from QCSchema.
+
+    Parameters
+    ----------
+    provenance
+
+    lit
+        The line iterator holding the file data.
+    source
+        The schema type {`qcschema_molecule`, `qcschema_input`, `qcschema_output`} associated
+        with this provenance data.
+    append
+        Append IOData provenance entry to provenance list?
+
+    """
+    if isinstance(provenance, dict):
+        if "creator" not in provenance:
+            raise FileFormatError(
+                "{}: `{}` provenance requires `creator` key".format(lit.filename, source)
+            )
+        else:
+            if append:
+                base_provenance = [provenance]
+            else:
+                return provenance
+    elif isinstance(provenance, list):
+        for prov in provenance:
+            if "creator" not in prov:
+                raise FileFormatError("{}: `{}` provenance requires `creator` key")
+        base_provenance = provenance
+    else:
+        raise FileFormatError("{}: Invalid `{}` provenance type".format(lit.filename, source))
+    if append:
+        base_provenance.append(
+            {"creator": "IOData", "version": __version__, "routine": "iodata.formats.json"}
+        )
+    return base_provenance
