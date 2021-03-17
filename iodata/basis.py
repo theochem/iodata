7c07c9be8d55b384890f48bedb6c6297357a8013
tovrstra <Toon.Verstraelen@UGent.be>
2021-03-17 11:11:58 +0100

    Replace PSI4_CONVENTIONS by CCA_CONVENTIONS

--- iodata/basis.py
+++ iodata/basis.py
@@ -30,7 +30,7 @@ from .attrutils import validate_shape
 
 __all__ = ['angmom_sti', 'angmom_its', 'Shell', 'MolecularBasis',
            'convert_convention_shell', 'convert_conventions',
-           'iter_cart_alphabet', 'HORTON2_CONVENTIONS', 'PSI4_CONVENTIONS']
+           'iter_cart_alphabet', 'HORTON2_CONVENTIONS', 'CCA_CONVENTIONS']
 
 ANGMOM_CHARS = 'spdfghiklmnoqrtuvwxyzabce'
 
@@ -341,20 +341,43 @@ def iter_cart_alphabet(n: int) -> np.ndarray:
             yield np.array((nx, ny, nz), dtype=int)
 
 
-def get_default_conventions():
-    """Produce a conventions dictionary compatible with HORTON2.
+def get_default_conventions() -> Tuple[Dict, Dict]:
+    """Produce conventions dictionaries compatible with HORTON2 and CCA.
 
-    Do not change this!!! This is also used by several file formats from other
-    QC codes who happen to follow the same conventions.
+    Do not change this! Both conventions are also used by several file formats
+    from other QC codes.
+
+    Common Component Architecture (CCA) conventions are defined in appendix B of
+    the following article:
+
+    Kenny, J. P.; Janssen, C. L.; Valeev, E. F.; Windus, T. L. Components for
+    Integral Evaluation in Quantum Chemistry: Components for Integral Evaluation
+    in Quantum Chemistry. J. Comput. Chem. 2008, 29 (4), 562–577.
+    https://doi.org/10.1002/jcc.20815.
+
+    The ordering of the spherical harmonics within one shell is rather vague
+    in appendix B and a more precise description is given on the LibInt Wiki:
+
+    https://github.com/evaleev/libint/wiki/using-modern-CPlusPlus-API
+
+    Returns
+    -------
+    horton2_conventions
+        A conventions dictionary for HORTON2, of which parts are used by various
+        file formats.
+    cca_conventions
+        A conventions dictionary compatible with the Common Component
+        Architecture (CCA).
+
     """
     horton2 = {(0, 'c'): ['1']}
-    psi4 = horton2.copy()
+    cca = horton2.copy()
     for angmom in range(1, 25):
         conv_cart = list('x' * nx + 'y' * ny + 'z' * nz
                          for nx, ny, nz in iter_cart_alphabet(angmom))
         key = (angmom, 'c')
         horton2[key] = conv_cart
-        psi4[key] = conv_cart
+        cca[key] = conv_cart
         if angmom > 1:
             conv_pure = ['c0']
             for absm in range(1, angmom + 1):
@@ -362,31 +385,8 @@ def get_default_conventions():
                 conv_pure.append('s{}'.format(absm))
             key = (angmom, 'p')
             horton2[key] = conv_pure
-            psi4[key] = conv_pure[:1:-2] + conv_pure[:1] + conv_pure[1::2]
-    return horton2, psi4
+            cca[key] = conv_pure[:1:-2] + conv_pure[:1] + conv_pure[1::2]
+    return horton2, cca
 
 
-HORTON2_CONVENTIONS, PSI4_CONVENTIONS = get_default_conventions()
-
-# fmt:off
-CCA_CONVENTIONS = {
-    (0, "c"): ["1"],
-    (1, "c"): ["x", "y", "z"],
-    (2, "c"): ["xx", "xy", "xz", "yy", "yz", "zz"],
-    (2, "p"): ["s2", "s1", "c0", "c1", "c2"],
-    (3, "c"): ["xxx", "xxy", "xxz", "xyy", "xyz", "xzz", "yyy", "yyz", "yzz", "zzz"],
-    (3, "p"): ["s3", "s2", "s1", "c0", "c1", "c2", "c3"],
-    (4, "c"): ["xxxx", "xxxy", "xxxz", "xxyy", "xxyz", "xxzz", "xyyy", "xyyz",
-               "xyzz", "xzzz", "yyyy", "yyyz", "yyzz", "yzzz", "zzzz"],
-    (4, "p"): ["s4", "s3", "s2", "s1", "c0", "c1", "c2", "c3", "c4"],
-    (5, "c"): ["xxxxx", "xxxxy", "xxxxz", "xxxyy", "xxxyz", "xxxzz", "xxyyy", "xxyyz",
-               "xxyzz", "xxzzz", "xyyyy", "xyyyz", "xyyzz", "xyzzz", "xzzzz", "yyyyy",
-               "yyyyz", "yyyzz", "yyzzz", "yzzzz", "zzzzz"],
-    (5, "p"): ["s5", "s4", "s3", "s2", "s1", "c0", "c1", "c2", "c3", "c4", "c5"],
-    (6, "c"): ["xxxxxx", "xxxxxy", "xxxxxz", "xxxxyy", "xxxxyz", "xxxxzz", "xxxyyy", "xxxyyz",
-               "xxxyzz", "xxxzzz", "xxyyyy", "xxyyyz", "xxyyzz", "xxyzzz", "xxzzzz", "xyyyyy",
-               "xyyyyz", "xyyyzz", "xyyzzz", "xyzzzz", "xzzzzz", "yyyyyy", "yyyyyz", "yyyyzz",
-               "yyyzzz", "yyzzzz", "yzzzzz", "zzzzzz"],
-    (6, "p"): ["s6", "s5", "s4", "s3", "s2", "s1", "c0", "c1", "c2", "c3", "c4", "c5", "c6"],
-}
-# fmt: on
+HORTON2_CONVENTIONS, CCA_CONVENTIONS = get_default_conventions()
