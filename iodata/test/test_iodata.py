cd95df1896514417eef80b747f42ddb4ecfdbbf8
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2024-05-22 11:51:11 +0200

    Add flake8-self rule

--- iodata/test/test_iodata.py
+++ iodata/test/test_iodata.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 # --
+# ruff: noqa: SLF001
 """Test iodata.iodata module."""
 
 import numpy as np
